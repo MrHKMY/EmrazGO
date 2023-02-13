8c0eb098692ba4d6a238dc34802c8fa26fce7715
Hakimi <mrhakimi88@gmail.com>
2023-02-13 14:27:12 +0800

    2nd changes

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       scrollBehavior: MyCustomScrollBehavior(),
-      home: SelectDateScreen(),
+      home: LoginScreen(),
       // home: ProvidedStylesExample(
       //   menuScreenContext: context,
       // ),
